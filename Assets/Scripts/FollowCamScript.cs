d6d841f8cc25f1328a6dd2e9aac2480d7a68b1d5
Max72590 <max-725@hotmail.com>
2018-03-09 22:29:54 +0100

    Updated CamScript now player can move around in a zone withouth the camera following.

--- Assets/Scripts/FollowCamScript.cs
+++ Assets/Scripts/FollowCamScript.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class FollowCamScript : MonoBehaviour {
 
     public GameObject player;
+    public float offsetX = 5.0f;
+    public float offsetZ = 5.0f;
+    public float smoothing = 10.0f;
     private Vector3 offset;
 
 	// Use this for initialization
@@ -16,6 +19,20 @@ public class FollowCamScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        gameObject.transform.position= player.transform.position + offset;
-	}
+        Vector3 playerPos = player.transform.position;
+        Vector3 pos = gameObject.transform.position;
+        Vector3 targetPos = playerPos+offset;
+        if (Mathf.Abs((pos-offset).x- playerPos.x) > offsetX)
+        {
+            //targetPos.x +=  offset.x;
+            gameObject.transform.position = Vector3.Lerp(pos, targetPos, smoothing * Time.deltaTime);
+        }
+        if (Mathf.Abs((pos - offset).z - playerPos.z) > offsetZ)
+        {
+            //targetPos.z +=  offset.z;
+            gameObject.transform.position = Vector3.Lerp(pos, targetPos, smoothing * Time.deltaTime);
+
+        }
+
+    }
 }
