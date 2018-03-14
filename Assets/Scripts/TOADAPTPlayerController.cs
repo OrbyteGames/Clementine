6385f3f35eaf4112149df2209a4824270905578c
Max72590 <max-725@hotmail.com>
2018-03-14 22:54:26 +0100

    Added assets and started building main scene.
    Problems with the player controller. RIGID BODIES AREN'T A GOOD IDEA
    USUALLY, clementine slides throught the floor. Main camera problems, it
    is not getting the associated cam.

--- Assets/Scripts/TOADAPTPlayerController.cs
+++ Assets/Scripts/TOADAPTPlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class TOADAPTPlayerController : MonoBehaviour {
 
     public float inputDelay = 0.1f;
     public float forwardVel = 12;
