4e54032467153eb8f7e41cfe17d133b34d39e342
Max72590 <max-725@hotmail.com>
2018-03-16 22:06:15 +0100

    UPdated main scene with new cams, bugs pending resolve.
    Camera switch to debug is a WIP. Panning cameras need to set position of
    the player when player enters inside them.

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+
+    public Camera debugCam;
+    public CameraManager camManager;
+    bool debugModeActive;
+	// Use this for initialization
+	void Start () {
+        debugModeActive = false;
+	}
+	
+
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.P)) {
+            if (!debugModeActive)
+            {
+                debugCam.transform.position = camManager.ActivedCamera.transform.position;
+                debugCam.transform.rotation = camManager.ActivedCamera.transform.rotation;
+                camManager.ActivedCamera.enabled = false;
+                debugCam.enabled = true;
+            }
+            else
+            {
+                debugCam.enabled = false;
+                camManager.ActivedCamera.enabled = true;
+            }
+        }
+    }
+}
