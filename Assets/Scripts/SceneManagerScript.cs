0b349deac292e7a9ed0162fe35728f54cd6bd8f8
Max72590 <max-725@hotmail.com>
2018-03-17 19:18:52 +0100

    Changed name of scenemanager script to avoid conflicting names with unity's classes

---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManagerScript : MonoBehaviour {
+
+    public Camera debugCam;
+    public CameraManager camManager;
+    bool debugModeActive;
+    public Camera[] cams;
+	// Use this for initialization
+	void Start () {
+        debugModeActive = false;
+        for (int i = 0; i < cams.Length; ++i)
+        {
+            cams[i].enabled = false;
+        }
+	}
+	
+
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.P)) {
+            if (!debugModeActive)
+            {
+                debugCam.transform.position = camManager.ActivedCamera.transform.position;
+                debugCam.transform.rotation = camManager.ActivedCamera.transform.rotation;
+                camManager.ActivedCamera.enabled = false;
+                debugCam.enabled = true;
+            }
+            else
+            {
+                debugCam.enabled = false;
+                camManager.ActivedCamera.enabled = true;
+            }
+        }
+    }
+}
