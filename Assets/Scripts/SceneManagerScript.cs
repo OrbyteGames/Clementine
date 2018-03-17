1f75f1579a0c127a687df46b4f944e3e0845d07c
Franky Pear <FrankPear85@hotmail.com>
2018-03-17 20:14:46 +0100

    //Adding save cam after push "P" for debugging.

--- Assets/Scripts/SceneManagerScript.cs
+++ Assets/Scripts/SceneManagerScript.cs
@@ -6,7 +6,9 @@ public class SceneManagerScript : MonoBehaviour {
 
     public Camera debugCam;
     public CameraManager camManager;
+    public Camera camSaved;
     bool debugModeActive;
+    int returnCamera;
     public Camera[] cams;
 	// Use this for initialization
 	void Start () {
@@ -21,18 +23,27 @@ public class SceneManagerScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.P)) {
+        if (Input.GetKeyDown(KeyCode.P)) {
             if (!debugModeActive)
             {
                 debugCam.transform.position = camManager.ActivedCamera.transform.position;
                 debugCam.transform.rotation = camManager.ActivedCamera.transform.rotation;
-                camManager.ActivedCamera.enabled = false;
+                //camManager.ActivedCamera.enabled = false;
+                for (int i = 0; i < cams.Length; ++i)
+                {
+                    if (cams[i].enabled) { returnCamera = i; camSaved = cams[i]; }
+                    cams[i].enabled = false;
+                }
                 debugCam.enabled = true;
+                debugModeActive = true;
             }
             else
             {
+                cams[returnCamera].enabled = true;
+                cams[returnCamera] = camSaved;
                 debugCam.enabled = false;
                 camManager.ActivedCamera.enabled = true;
+                debugModeActive = false;
             }
         }
     }
