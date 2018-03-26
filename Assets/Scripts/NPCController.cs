90477946da31124b4d84582a2c18a231ff5c28d6
OscarLopezFabregas <oscar.lopez6@alumni.esade.edu>
2018-03-26 19:58:10 +0200

    Created Prefab that moves between 2 points

---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCController : MonoBehaviour {
+
+    public Transform origin;
+    public Transform destination;
+
+    public NavMeshAgent agent;
+	// Use this for initialization
+	void Start ()
+    {
+        transform.position = origin.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float dist = Vector3.Distance(transform.position, origin.position);
+        if(dist < 2f)
+            agent.SetDestination(destination.position);
+       float  dist2 = Vector3.Distance(transform.position, destination.position);
+        if (dist2 < 2f)
+            agent.SetDestination(origin.position);
+
+             
+	}
+}
