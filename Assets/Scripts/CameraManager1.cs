9823a629eec43eef2889e61b76be6516b62431e9
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-04-04 20:09:25 +0200

    created camera manager

--- Assets/Scripts/CameraManager1.cs
+++ Assets/Scripts/CameraManager1.cs
@@ -7,13 +7,13 @@ public class CameraManager1 : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-        //Assuming parent is the parent game object
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            var child = transform.GetChild(i).gameObject;
-            if (child != null)
-                child.SetActive(false);
-        }
+        ////Assuming parent is the parent game object
+        //for (int i = 0; i < transform.childCount; i++)
+        //{
+        //    var child = transform.GetChild(i).gameObject;
+        //    if (child != null)
+        //        child.SetActive(false);
+        //}
     }
 	
 	// Update is called once per frame
