a1b1ea998776032880ffd554af5c533b46c571ec
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-04-04 09:57:04 +0200

    Clemen moves

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 6.0f;
+    public float gravity = 10.0f;
+    public float turnSmooth = 15.0f;
+    public bool airFlight = true;
+    public float jumpforce = 14.0f;
+
+    private bool start;
+    private CharacterController playerController;
+    private Camera actualCamera;
+    private float verticalVelocity;
+    // Use this for initialization
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+
+        transform.Translate(x, z, 0);
+    }
+}
