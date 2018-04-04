8134040f1bbc7f7689b0a9af7c92e5aa9f713827
Alex <alexespinet@gmail.com>
2018-04-04 20:34:55 +0200

    Backup

---
+++ Assets/Scripts/Camera/AutoActiveCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoActiveCamera : MonoBehaviour
+{
+
+    [SerializeField]
+    Camera _attachedCamera;
+
+    private void Start()
+    {
+        if (_attachedCamera == null)
+            throw new System.Exception("You must attach a camera");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _attachedCamera.gameObject.SetActive(true);
+    }
+}
