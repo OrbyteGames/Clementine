ba91c5d36e2783801de58e9cc421ddd8d4628b6c
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-04-04 20:08:51 +0200

    added starting animation, improved quality settings
    missing initial script

---
+++ Assets/Scripts/CameraManager1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Assuming parent is the parent game object
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i).gameObject;
+            if (child != null)
+                child.SetActive(false);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
