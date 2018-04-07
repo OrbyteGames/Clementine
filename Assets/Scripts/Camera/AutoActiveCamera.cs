0c0671d97f74f120126ee3707e5e1193ec38e931
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-04-07 13:29:41 +0200

    Addaptative Axis towards movement. Axis change when control is released

--- Assets/Scripts/Camera/AutoActiveCamera.cs
+++ Assets/Scripts/Camera/AutoActiveCamera.cs
@@ -7,25 +7,49 @@ public class AutoActiveCamera : MonoBehaviour
 
     [SerializeField]
     Camera _attachedCamera;
+    public Transform forwardPoint;
+    public GameObject clement;
+    bool hasChanged = true;
     bool hasExit = false;
 
     private void Start()
     {
         if (_attachedCamera == null)
             throw new System.Exception("You must attach a camera");
+        clement.transform.forward = forwardPoint.forward;
     }
+    private void Update()
+    {
 
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0 
+            && hasChanged == false )
+        {
+            hasChanged = true;
+            clement.transform.forward = forwardPoint.forward;
+             Debug.Log( forwardPoint.gameObject.name);
+          
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Entered: " + gameObject.name);
-        _attachedCamera.gameObject.SetActive(true);
-        if(CameraManager.Instance!=null)
-            CameraManager.Instance.SwitchCamera(_attachedCamera);
-       
-        else
+        if(other.tag == "Player")
         {
-            throw new System.Exception("You must add Camera manager script in the scene");
+            Debug.Log("Entered: " + gameObject.name);
+            _attachedCamera.gameObject.SetActive(true);
+            hasChanged = false;
+            if (CameraManager.Instance != null)
+            {
+                CameraManager.Instance.SwitchCamera(_attachedCamera);
+               
+                return;
+            }
+
+            else
+            {
+                throw new System.Exception("You must add Camera manager script in the scene");
+            }
         }
+      
     }
     private void OnTriggerExit(Collider other)
     {
---
+++ Assets/Scripts/ControlTemp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlTemp : MonoBehaviour {
+
+    public float speed = 3f;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        float postX = Input.GetAxis("Horizontal");
+        float PosY = Input.GetAxis("Vertical");
+
+        transform.Translate(postX*speed*Time.deltaTime, 0f, PosY*speed*Time.deltaTime);
+     
+	}
+}
