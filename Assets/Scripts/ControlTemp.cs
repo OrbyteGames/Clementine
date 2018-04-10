5bc436267e1cb3cbf8b28b368f884573d0fa06fb
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-04-10 16:32:58 +0200

    UI

--- Assets/Scripts/ControlTemp.cs
+++ Assets/Scripts/ControlTemp.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class ControlTemp : MonoBehaviour {
 
     public float speed = 3f;
-	
-	// Update is called once per frame
-	void Update ()
+    MenuManager menu;
+    public GameObject pausePanel;
+    public GameObject UI;
+    // Update is called once per frame
+    void Update ()
     {
         
         float postX = Input.GetAxis("Horizontal");
         float PosY = Input.GetAxis("Vertical");
 
         transform.Translate(postX*speed*Time.deltaTime, 0f, PosY*speed*Time.deltaTime);
-     
-	}
+
+        if (Input.GetKey("p"))
+        {
+            menu.Pause(this.gameObject);
+            menu.ActivatePanel(pausePanel);
+        }
+
+    }
 }
---
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    private void Start()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void ActivatePanel(GameObject panel)
+    {
+        panel.SetActive(true);
+
+    }
+
+    public void Back(GameObject actualPanel)
+    {
+        actualPanel.SetActive(false);
+    }
+
+    public void Play(GameObject player)
+    {
+        player.SetActive(true);
+        Time.timeScale = 1f;
+    }
+    //Check OnApplicationPause and how it works
+    public void Pause(GameObject player)
+    {
+        Time.timeScale = 0f;
+        player.SetActive(false);
+    }
+}
