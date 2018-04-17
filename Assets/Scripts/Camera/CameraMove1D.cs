157a7a494ab82fb90c1797fd149751a4a841bd64
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-04-17 16:54:02 +0200

    added more scripts to set up cameras

--- Assets/Scripts/Camera/CameraMove1D.cs
+++ Assets/Scripts/Camera/CameraMove1D.cs
@@ -3,21 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[RequireComponent(typeof(Rigidbody))]
 public class CameraMove1D : MonoBehaviour {
     public GameObject clemen;
-
-    Rigidbody rb;
-
+    public bool x=false, y=false, z = false;
+    public float step = 10f;
+    public float distanceX = 0f, distanceY = 0f, distanceZ = 5f;
     // Use this for initialization
     void Start ()
     {
-        rb = GetComponent<Rigidbody>();
-        this.transform.SetParent(clemen.transform);
-        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ
-            | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY;
+     
+    }
 
+    private void Update()
+    {
+      if(x)
+        {
+            transform.position = new Vector3(Mathf.MoveTowards(transform.position.x, clemen.transform.position.x - distanceX, step * Time.deltaTime), transform.position.y, transform.position.z);
+        }
+      if(z)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.MoveTowards(transform.position.z, clemen.transform.position.z-distanceZ, step * Time.deltaTime));
+
+        }
+      else
+        {
+            transform.position = new Vector3(transform.position.x, Mathf.MoveTowards(transform.position.y, clemen.transform.position.y - distanceY, step * Time.deltaTime), transform.position.z);
+
+        }
 
     }
-	
+
 }
