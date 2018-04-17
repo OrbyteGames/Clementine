53508ba54c805b9dac0d6c82fde917309c6dc792
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-04-17 16:13:21 +0200

    added some scripts

---
+++ Assets/Scripts/Camera/CameraMove1D.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class CameraMove1D : MonoBehaviour {
+    public GameObject clemen;
+
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        this.transform.SetParent(clemen.transform);
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ
+            | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY;
+
+
+    }
+	
+}
---
+++ Assets/Scripts/Camera/CameraPanning.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPanning : MonoBehaviour {
+
+    public GameObject clement;
+    Camera cam;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cam = GetComponent<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        cam.transform.LookAt(clement.transform);	
+	}
+}
