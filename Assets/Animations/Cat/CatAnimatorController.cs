2b301f39ed415bcb1fff2556dcf2157480e30783
Franky Pear <FrankPear85@hotmail.com>
2018-04-22 00:00:07 +0200

    Cat Animations first part

---
+++ Assets/Animations/Cat/CatAnimatorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatAnimatorController : MonoBehaviour
+{
+
+    Animator anim;
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("1"))
+        {
+            anim.SetBool("idle", true);
+            anim.SetBool("walk", false);
+        }
+        if (Input.GetButtonDown("2"))
+        {
+            anim.SetBool("idle", false);
+            anim.SetBool("walk", true);
+
+        }
+    }
+}
