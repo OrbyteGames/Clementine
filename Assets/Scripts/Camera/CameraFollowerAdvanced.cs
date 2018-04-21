84f444f9e31502b12044b05657bfaea0f39f2ac2
Alex <alexespinet@gmail.com>
2018-04-21 21:38:08 +0200

    Camera Follower

---
+++ Assets/Scripts/Camera/CameraFollowerAdvanced.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowerAdvanced : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform _target;
+
+
+    [Range(0f, 1f), SerializeField]
+    float _smoothness = 0.5f;
+
+    [SerializeField]
+    bool _isFixed = true;
+
+    Vector3 _positionOffset;
+    Quaternion _rotationOffset;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if (_target == null)
+            _target = GameObject.FindGameObjectWithTag("Player").transform;
+        _positionOffset = transform.position-_target.position;
+        _rotationOffset = transform.rotation;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_isFixed)
+        {
+            transform.position = Vector3.Slerp(transform.position, _target.position + _positionOffset, _smoothness);
+        }
+        else
+        {
+            Vector3 nextRotation= Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(_target.position - transform.position),_smoothness).eulerAngles;
+            nextRotation.x = 0;
+            transform.rotation = Quaternion.Euler(nextRotation);
+
+
+        }
+
+
+    }
+}
