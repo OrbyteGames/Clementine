cb2950aaaa6189ab8fe5142a07a97f38832ce7f1
Max72590 <max-725@hotmail.com>
2018-05-23 19:28:35 +0200

    Replaced container and prepared more test for puzzle 3

--- Assets/Scripts/Player/SimpleTestPlayerMovement.cs
+++ Assets/Scripts/Player/SimpleTestPlayerMovement.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class SimpleTestPlayerMovement : MonoBehaviour {
 
+
+    bool grounded;
 	// Use this for initialization
 	void Start () {
-		
+        grounded = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-
+        
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+        if (!grounded)
+        {
+            transform.Translate(0, -1f * Time.deltaTime, 0);
+        }
+        if (!GetComponent<CharacterController>().isGrounded)
+        {
+            grounded = false ;
+        }
+        else
+        {
+            grounded = true;
+        }
     }
 }
